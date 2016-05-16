543b8f2703b6128bbf71148a26fbdf0abfdfa785
Hyunho Kang <hhstark.kang@samsung.com>
2016-05-16 20:25:59 +0900

    Fix dereferenced NULL
    Change-Id: I6793dcb34244fd6f2d7dca7c293aa99e1ee32659
    Signed-off-by: Hyunho Kang <hhstark.kang@samsung.com>

--- src/data-control-provider.c
+++ src/data-control-provider.c
@@ -418,7 +418,7 @@ int __datacontrol_send_async(int sockfd, bundle *kb, datacontrol_request_type ty
 	bundle_raw *kb_data = NULL;
 	int ret = DATACONTROL_ERROR_NONE;
 	int datalen = 0;
-	char *buf = NULL;
+	char *buf;
 	int total_len = 0;
 	unsigned int nb = 0;
 
@@ -432,6 +432,10 @@ int __datacontrol_send_async(int sockfd, bundle *kb, datacontrol_request_type ty
 
 	/* encoded bundle + encoded bundle len */
 	buf = (char *)calloc(datalen + 4, sizeof(char));
+	if (buf == NULL) {
+		LOGE("buf calloc error");
+		return DATACONTROL_ERROR_OUT_OF_MEMORY;
+	}
 	memcpy(buf, &datalen, sizeof(datalen));
 	memcpy(buf + sizeof(datalen), kb_data, datalen);
 
@@ -450,8 +454,7 @@ int __datacontrol_send_async(int sockfd, bundle *kb, datacontrol_request_type ty
 		ret = __send_get_value_result(sockfd, kb, data);
 
 out:
-	if (buf)
-		free(buf);
+	free(buf);
 	bundle_free_encoded_rawdata(&kb_data);
 
 	return ret;
